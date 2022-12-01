fccc131d14ceb5bd65df72829e9b2bd9b7cd3e74
CatalinaML <listecatalina7@gmail.com>
2022-11-30 21:11:00 -0300

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -2,7 +2,7 @@
 
     define("ROOT", dirname(__DIR__) . "/");
     //Path to your project's root folder
-    define("FRONT_ROOT", "/CondarcoFest/"); // cambiar
+    define("FRONT_ROOT", "/CondarcoFest/"); 
     define("VIEWS_PATH", "Views/");
     define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
     define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
