a63e3084e1493ac9697f0d59c1f40d6234fbfbcf
CatalinaML <listecatalina7@gmail.com>
2022-11-30 21:11:26 -0300

    footer y header listos

---
+++ Views/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p class="p_footer">Club Banco Provincia de Mar del Plata | | | Propiedad de Liste, Catalina </p>
+</footer>
+</body>
+
+</html>
---
+++ Views/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="<?php echo CSS_PATH . 'basic.css';?>">
+
+    <title>CondarcoFest</title>
+</head>
+
+<body>
+  
+  <header>
+    <h1 class="name_header">BANCO PROVINCIA</h1>
+    <img src="<?php echo IMG_PATH . 'escudo.png' ?>" alt="Logo">
+  </header>
