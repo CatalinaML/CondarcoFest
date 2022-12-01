ce82f356547c1d7d65010d36a0fcaea656e8971c
CatalinaML <listecatalina7@gmail.com>
2022-11-30 22:45:32 -0300

    Update inicio.php

--- Views/inicio.php
+++ Views/inicio.php
@@ -1,4 +1,8 @@
-<?php require_once(VIEWS_PATH . 'header.php'); ?>
+<?php
+
+use DAO\EventDAO;
+
+ require_once(VIEWS_PATH . 'header.php'); ?>
 
 <main>
     <div>
@@ -9,14 +13,27 @@
     </div>
     
     <!--if existe algo!-->
-    <div>
-        <h1><!--Nombre evento!--></h1>
+    <?php
+    $eventsDAO = new EventDAO();
+    $listEvents = $eventsDAO->getAll();
+    if($listEvents != null){ 
+        foreach($listEvents as $event){ ?>
+        <div>
+        <h1><?php echo $event->getName();?></h1>
+        <h2>Fecha: <?php echo $event->getDate();?></h2>
+        <h2>Valor entrada: <?php echo $event->getPrice();?></h2>
+
+        <form action="<?php echo FRONT_ROOT . 'Home/showListGuest'?>" method="post">
+            <button class="button-28" role="button">Invitados</button>
+        </form>
 
         <form action="" method="post">
-            <button class="button-28" role="button">Invitados</button>
             <button class="button-28" role="button">Check In</button>
         </form>
     </div>
+    <?php }
+     } ?>
+    
 
 </main>
 
